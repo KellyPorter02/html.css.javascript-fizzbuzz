f6eb6fbabaf1559622105c682da66879f8e8b25c
Kelly Porter <porter.kelly02@gmail.com>
2023-06-06 19:51:20 -0400

    exercise complete

--- js/main/FizzBuzz.js
+++ js/main/FizzBuzz.js
@@ -5,19 +5,43 @@ class FizzBuzz {
     }
   
     isFizz(num) {
-        return null;
+        if (num % this.fizz == 0) {
+            return true;
+        } else {
+            return false;
+        }  
     }
   
     isBuzz(num) {
-        return null;
+        if (num % this.buzz == 0) {
+            return true;
+        } else {
+            return false;
+        } 
     }
   
     isFizzBuzz(num) {
-        return null;
+        if (num % this.fizz == 0 && num % this.buzz == 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
   
     getOutput(num) {
-        return null;
+        let output = "FizzBuzz ";
+        for (let i=1; i<=num; i++) {
+            if (this.isFizzBuzz(i)) {
+                output += "FizzBuzz ";
+            } else if (this.isFizz(i)) {
+                output += "Fizz ";
+            } else if (this.isBuzz(i)) {
+                output += "Buzz "
+            } else {
+                output += i + " ";
+            }
+        }
+        return output;
     }
   }
   
